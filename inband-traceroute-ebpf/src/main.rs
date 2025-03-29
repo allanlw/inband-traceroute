4e10de156b7e9ed58974b0d2177892daf30c1b3d
Allan Wirth <allan@allanwirth.com>
2025-03-29 18:37:57 +0000

    Remove unnecessary unsafe

--- inband-traceroute-ebpf/src/main.rs
+++ inband-traceroute-ebpf/src/main.rs
@@ -17,10 +17,6 @@ const IPV6_LISTEN_ADDR: [u32; 4] = [1638438, 1644253248, 0, 0]; // 2600:1900:405
 
 #[xdp]
 pub fn inband_traceroute(ctx: XdpContext) -> u32 {
-    unsafe {
-        bpf_printk!(b"received a packet1");
-    }
-
     match try_inband_traceroute(ctx) {
         Ok(_) => xdp_action::XDP_PASS,
         Err(_) => xdp_action::XDP_ABORTED,
@@ -28,30 +24,30 @@ pub fn inband_traceroute(ctx: XdpContext) -> u32 {
 }
 
 fn try_inband_traceroute(ctx: XdpContext) -> Result<(), ()> {
-    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
-    let ether_type = unsafe { (*ethhdr).ether_type };
+    let ethhdr: &EthHdr = ptr_at(&ctx, 0)?;
+    let ether_type = ethhdr.ether_type;
 
     let mut layer4_protocol: Option<IpProto> = None;
     let mut layer4_offset: Option<usize> = None;
 
     match ether_type {
         EtherType::Ipv4 => {
-            let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
-            let dst_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
+            let ipv4hdr: &Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+            let dst_addr = u32::from_be(ipv4hdr.dst_addr);
             if dst_addr != IPV4_LISTEN_ADDR {
                 return Ok(());
             }
 
-            layer4_protocol = Some(unsafe { (*ipv4hdr).proto });
+            layer4_protocol = Some(ipv4hdr.proto);
             layer4_offset = Some(EthHdr::LEN + Ipv4Hdr::LEN);
         }
         EtherType::Ipv6 => {
-            let ipv6hdr: *const Ipv6Hdr = ptr_at(&ctx, EthHdr::LEN)?;
-            if unsafe { (*ipv6hdr).dst_addr.in6_u.u6_addr32 } != IPV6_LISTEN_ADDR {
+            let ipv6hdr: &Ipv6Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+            if unsafe { ipv6hdr.dst_addr.in6_u.u6_addr32 } != IPV6_LISTEN_ADDR {
                 return Ok(());
             }
 
-            layer4_protocol = Some(unsafe { (*ipv6hdr).next_hdr });
+            layer4_protocol = Some(ipv6hdr.next_hdr);
             layer4_offset = Some(EthHdr::LEN + Ipv6Hdr::LEN);
         }
         _ => {
@@ -61,8 +57,8 @@ fn try_inband_traceroute(ctx: XdpContext) -> Result<(), ()> {
 
     match layer4_protocol {
         Some(IpProto::Tcp) => {
-            let tcp_hdr: *const TcpHdr = ptr_at(&ctx, layer4_offset.unwrap())?;
-            let dst_port = u16::from_be(unsafe { (*tcp_hdr).dest });
+            let tcp_hdr: &TcpHdr = ptr_at(&ctx, layer4_offset.unwrap())?;
+            let dst_port = u16::from_be(tcp_hdr.dest);
             if dst_port != PORT {
                 return Ok(());
             }
@@ -86,7 +82,7 @@ fn try_inband_traceroute(ctx: XdpContext) -> Result<(), ()> {
 }
 
 #[inline(always)]
-fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<&T, ()> {
     let start = ctx.data();
     let end = ctx.data_end();
     let len = mem::size_of::<T>();
@@ -95,14 +91,12 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
         return Err(());
     }
 
-    Ok((start + offset) as *const T)
+    // Safety: Verified to be in bounds by the above check
+    Ok(unsafe { &*((start + offset) as *const T) })
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe {
-        bpf_printk!(b"PANIC");
-    }
     loop {}
 }
