4cd4e1ea66c364850e0a75435d5f8c6ed946c072
Allan Wirth <allan@allanwirth.com>
2025-03-30 10:35:14 +0000

    refactor: Simplify Tracer struct and constructor for raw socket usage

--- inband-traceroute/src/tracer.rs
+++ inband-traceroute/src/tracer.rs
@@ -1,28 +1,22 @@
+mod raw;
+
+use libc::{IPPROTO_RAW, SOCK_RAW};
 use socket2::{Domain, Protocol, Socket, Type};
-use std::net::IpAddr;
+use std::net::{IpAddr, SocketAddr};
 use tokio::net::UdpSocket;
 
 pub struct Tracer<IP: Into<IpAddr> + Copy> {
-    pub ip: IP,
-    pub port: u16,
+    pub listen_addr: SocketAddr,
     pub max_hops: u8,
-    pub socket: UdpSocket,
+    pub socket: raw::AsyncWriteOnlyIPRawSocket,
 }
 
-impl<IP: Into<IpAddr>> Tracer<IP> {
-    pub async fn new(ip: IP, port: u16, max_hops: u8) -> tokio::io::Result<Self> {
-        let domain = match ip.into() {
-            IpAddr::V4(_) => Domain::IPV4,
-            IpAddr::V6(_) => Domain::IPV6,
-        };
-        let socket = Socket::new(domain, Type::DGRAM, Some(Protocol::UDP))?;
-        socket.set_nonblocking(true)?;
-        let std_socket = std::net::UdpSocket::from(socket);
-        let socket = UdpSocket::from_std(std_socket)?;
+impl<IP: Into<IpAddr> + std::marker::Copy> Tracer<IP> {
+    pub fn new(listen_addr: SocketAddr, max_hops: u8) -> anyhow::Result<Self> {
+        let socket = raw::AsyncWriteOnlyIPRawSocket::new(domain)?;
 
-        Ok(Tracer {
-            ip,
-            port,
+        Ok(Self {
+            listen_addr,
             max_hops,
             socket,
         })
