10e8b4c9c08bc4ecd0246530b9e287d777e23f77
Allan Wirth <allan@allanwirth.com>
2025-05-09 17:11:05 +0900

    Remove scaleway from terraform

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,10 @@
 locals {
-  dev_mode = "none" # "vultr", "scaleway", "none"
+  dev_mode = "none" # "vultr", "none"
 
   # derived locals
-  dev_ipv4       = local.dev_mode == "vultr" ? vultr_instance.dev[0].main_ip : local.dev_mode == "scaleway" ? scaleway_instance_ip.dev_ipv4[0].address : null
-  dev_ipv6       = local.dev_mode == "vultr" ? vultr_instance.dev[0].v6_main_ip : local.dev_mode == "scaleway" ? scaleway_instance_ip.dev_ipv6[0].address : null
-  instance_id    = local.dev_mode == "vultr" ? vultr_instance.dev[0].id : local.dev_mode == "scaleway" ? scaleway_instance_server.dev[0].id : null
+  dev_ipv4       = local.dev_mode == "vultr" ? vultr_instance.dev[0].main_ip : null
+  dev_ipv6       = local.dev_mode == "vultr" ? vultr_instance.dev[0].v6_main_ip : null
+  instance_id    = local.dev_mode == "vultr" ? vultr_instance.dev[0].id : null
   dev_short_hash = local.dev_mode != "none" ? substr(sha256(local.instance_id), 0, 4) : "foo"
   dev_domain     = "dev-${local.dev_short_hash}.inband-traceroute.net."
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "vultr/vultr"
       version = "2.26.0"
     }
-    scaleway = {
-      source  = "scaleway/scaleway"
-      version = "2.53.0"
-    }
     github = {
       source  = "integrations/github"
       version = "6.6.0"
