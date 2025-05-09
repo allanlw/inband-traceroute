38751a1b03e2728f2c93f221b09522dc3cf907e5
Allan Wirth <allan@allanwirth.com>
2025-05-09 10:39:01 +0000

    Update ipdb.sh

---
+++ update-ipdb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TOKEN="$(cat /ipinfoio_token)"
+OUTPUT_FILE="/opt/ipinfoio/ipinfo_lite.mmdb"
+
+if [ -f "$OUTPUT_FILE" ]; then
+    # Check if file is older than 24 hours (86400 seconds)
+    if [ $(($(date +%s) - $(stat -c %Y "$OUTPUT_FILE"))) -lt 86400 ]; then
+        echo "File is up to date. Skipping download."
+        exit 0
+    fi
+fi
+
+curl https://ipinfo.io/data/ipinfo_lite.mmdb?token="${TOKEN}" \
+  -o "$OUTPUT_FILE"
