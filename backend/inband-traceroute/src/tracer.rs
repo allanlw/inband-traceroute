a6399b144637c46d2421c91bb10897161bc1aa22
Allan Wirth <allan@allanwirth.com>
2025-05-10 22:10:45 +0900

    Reduce timeout to 1 second

--- backend/inband-traceroute/src/tracer.rs
+++ backend/inband-traceroute/src/tracer.rs
@@ -32,7 +32,7 @@ use crate::{
 
 type TraceId = u32;
 
-const STEP_TIMEOUT: Duration = Duration::from_secs(5);
+const STEP_TIMEOUT: Duration = Duration::from_secs(1);
 
 #[derive(Debug)]
 pub struct Tracer {
